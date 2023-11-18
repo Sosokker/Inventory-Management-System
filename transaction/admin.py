58fa1b378e75cd7c8d6bdb1869297829fdaf7146
sosokker <sirin_29@hotmail.co.th>
2023-11-18 19:06:50 +0700

    Register transaction model to admin

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from transaction.models import Supplier, Supply, Customer, Order, Transfer
 
-# Register your models here.
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+
+@admin.register(Supply)
+class SupplyAdmin(admin.ModelAdmin):
+    list_display = ('supplier', 'item', 'quantity', 'arrive_date')
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'item', 'order_date', 'quantity')
+
+@admin.register(Transfer)
+class TransferAdmin(admin.ModelAdmin):
+    list_display = ('from_warehouse', 'to_warehouse', 'item', 'from_date_timestamp', 'to_date_timestamp', 'quantity')
