a8add047d079e9c8c67b3760052be3773be8802d
sosokker <sirin_29@hotmail.co.th>
2023-11-18 16:54:16 +0700

    Register inventory model to admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from inventory.models import Warehouse, Inventory, Item
 
-# Register your models here.
+@admin.register(Warehouse)
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'have_freeze')
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('warehouse', 'max_stock', 'min_stock', 'current_stock')
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('inventory', 'name', 'description', 'category', 'weight', 'quantity')
